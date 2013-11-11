a73b247301be74dfb663a77718ba55815cf5a691
Ken L <klauer@bnl.gov>
2013-11-11 17:41:29 -0500

    PMD90 st.cmd fix / serial conn

--- iocBoot/iocPMD90/st.cmd
+++ iocBoot/iocPMD90/st.cmd
@@ -10,23 +10,33 @@ epicsEnvSet "R" "$(R=PMD90:)"
 epicsEnvSet "PMD90_IP" "$(PMD90_IP=10.0.0.11)"
 epicsEnvSet "PMD90_PORT" "$(PMD90_PORT=4015)"
 
+epicsEnvSet "ASYN_PORT" "PMD90"
+
 ## Register all support components
 dbLoadDatabase("$(TOP)/dbd/PMD90.dbd",0,0)
 PMD90_registerRecordDeviceDriver(pdbbase) 
 
 ###############################################################################
 # Set up ASYN ports
-drvAsynIPPortConfigure("pmd90_port","$(PMD90_IP):$(PMD90_PORT)",0,0,0)
-asynOctetSetInputEos("pmd90_port", -1, "\n")
-asynOctetSetOutputEos("pmd90_port", -1, "\n")
-asynSetTraceIOMask("pmd90_port",-1,0)
-asynSetTraceMask("pmd90_port",-1,0)
-#asynSetTraceIOMask("pmd90_port",-1,0x2)
-#asynSetTraceMask("pmd90_port",-1,0x9)
+drvAsynIPPortConfigure("$(ASYN_PORT)","$(PMD90_IP):$(PMD90_PORT)",0,0,0)
+
+## for direct connection to the machine's serial port:
+# drvAsynSerialPortConfigure("$(ASYN_PORT)", "/dev/ttyUSB0", 0, 0, 0)
+# asynSetOption("$(ASYN_PORT)",0,"baud","9600")
+# asynSetOption("$(ASYN_PORT)",0,"parity","none")
+# asynSetOption("$(ASYN_PORT)",0,"bits","8")
+# asynSetOption("$(ASYN_PORT)",0,"stop","1")
+
+asynOctetSetInputEos("$(ASYN_PORT)", -1, "\n")
+asynOctetSetOutputEos("$(ASYN_PORT)", -1, "\n")
+asynSetTraceIOMask("$(ASYN_PORT)",-1,0)
+asynSetTraceMask("$(ASYN_PORT)",-1,0)
+#asynSetTraceIOMask("$(ASYN_PORT)",-1,0x2)
+#asynSetTraceMask("$(ASYN_PORT)",-1,0x9)
 
 ###############################################################################
 ## Load record instances
-dbLoadRecords("$(TOP)/db/devPMD90.db","user=nanopos,P=$(P),R=$(R),PORT=pmd90_port,A=0")
+dbLoadRecords("$(TOP)/db/devPMD90.db","P=$(P),R=$(R),PORT=$(ASYN_PORT),A=0")
 
 iocInit()
 #var streamDebug 1
